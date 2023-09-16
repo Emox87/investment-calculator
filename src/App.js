64af3822dce2bfadff2621790717093852cd99ca
Emil Tsvetanov <tsvetanovemil87@gmail.com>
2023-09-16 13:36:52 +0300

    Adding and styling UserInput Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import UserInput from "./components/UserInput/UserInput";
 
 const App = () => {
   return (
     <div>
       <Header />
+      <UserInput />
     </div>
   );
 };
